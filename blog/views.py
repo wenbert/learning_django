67c7c8ca24acfb22826fba5cdb8d38364d532912
wdelrosa <wdelrosa@wdelrosa-ubuntu.(none)>
2009-09-18 18:12:52 +0800

    using count()

--- blog/views.py
+++ blog/views.py
@@ -5,16 +5,15 @@ from mysite.blog.models import Comment
 
 def index(request):
     blog_posts = Blog.objects.all().order_by('-pub_date')[:5]
+    
     return render_to_response('blog/index.html', {'blog_posts': blog_posts})
 
 def post(request,blog_id):
     try:
         post = Blog.objects.get(pk=blog_id)
-        #Below, I want to get the comments
-        #----------------------------------------------------------
         comments = Comment.objects.filter(blog=post)
-        #----------------------------------------------------------
-
+        comment_count = comments.count()
     except Blog.DoesNotExist:
         raise Http404
-    return render_to_response('blog/post.html', {'post': post, 'comments': comments})
+    return render_to_response('blog/post.html', 
+                              {'post': post, 'comments': comments, 'comment_count': comment_count})
